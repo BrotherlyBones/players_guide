8d6393b326e8f4f9295de34a07a6e3e0ade422b4
BrotherlyBones <dfmicklei@gmail.com>
2024-09-18 12:35:35 +0200

    update to work(?)

---
+++ src/helloother/helloother.java
@@ -0,0 +1,8 @@
+package helloother;
+
+public class helloother {
+    public static void main(String[] args) {
+        System.out.println("Okay, Fine. If you really wish for me to do something as basic as changing the message contained between the quote characters;");
+        System.out.println("Here you Go.");
+    }
+}
--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -1,7 +1,7 @@
 package helloworld;
 
 public class helloworld {
-    public static void HelloWorld(){
+    public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 }

80d40b81bf46b845e749fe386c8a947583189728
BrotherlyBones <dfmicklei@gmail.com>
2024-09-18 12:49:34 +0200

    simply keeping the repository up to date with my work

---
+++ src/Printmultilines/printmultilines.java
@@ -0,0 +1,12 @@
+package Printmultilines;
+
+public class printmultilines {
+    public static void main(String[] args){
+        System.out.println("this exercise asks a question");
+        System.out.println("it would be most fitting for me to answer it in full");
+        System.out.println("The question reads: \"How many statements do you think a program can contain?\"");
+        System.out.println("My answer won't be so simple, due to me already having a partial understanding of the truth");
+        System.out.println("that being, a program can have as many statements as it desires.");
+        System.out.println("As long as none of said statements throw an exception, cause a system crash or exhibit other types of unwanted or unwarranted behaviour.");
+    }
+}

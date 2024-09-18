d0a90e5fc18dec1b66497b808b90a8e64ff3d63e
BrotherlyBones <dfmicklei@gmail.com>
2024-09-18 14:17:25 +0200

    simply keeping the repository up to date with my work

---
+++ src/six_eightvars/eightvars.java
@@ -0,0 +1,19 @@
+package six_eightvars;
+
+public class eightvars {
+    public static void main(String[] args){
+        byte crushed = 100;
+        short cut = 10*1000;
+        int elligent = 1000*1000*1000;
+        long john = 1000000000000000000L;
+        float boat = 0.000001f;
+        double trouble = 0.000000000000001;
+        boolean schoolean = false;
+        char acter = '☻';
+        String puppet = "Wait a minute, why is there just one zero in this:";
+        System.out.println(schoolean);
+        System.out.println(acter);
+        System.out.println(puppet);
+        System.out.println(trouble*john*elligent*boat*cut*crushed);
+    }
+}

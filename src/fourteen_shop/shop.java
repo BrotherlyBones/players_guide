56757607907f946d105ccd938c8ec226fb4d916f
BrotherlyBones <dfmicklei@gmail.com>
2024-09-18 16:07:59 +0200

    simply keeping the repository up to date with my work

---
+++ src/fourteen_shop/shop.java
@@ -0,0 +1,18 @@
+package fourteen_shop;
+
+/*
+first idea: array
+put every object in an array as a string and ask for which to target
+include a way to back out of a decision
+make it so the first letter of each word in the item is not case-sensitive by including OR statements
+include comments for where to call other classes' methods such as deducting cash and displaying the inventory/
+
+assignment requires i use a switch statement,
+shouldn't be too hard and is probably exactly what i was thinking of doing anyway
+ */
+
+public class shop {
+    public static void main(String[] args){
+
+    }
+}

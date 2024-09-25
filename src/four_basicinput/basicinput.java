5504aa73eec29aad44a208f46d36bbb6a0357b7b
BrotherlyBones <dfmicklei@gmail.com>
2024-09-25 11:30:51 +0200

    simply keeping the repository up to date with my work

--- src/four_basicinput/basicinput.java
+++ src/four_basicinput/basicinput.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class basicinput {
     public static void main(String[] args){
-    Scanner scannerObj = new Scanner(System.in);
-    System.out.println("Who thinks they're really funny?");
-    String funnyman = scannerObj.nextLine();
-    System.out.println("Look out guys! " + funnyman + " thinks they're funny!!!");
+        Scanner scannerObj = new Scanner(System.in);
+        System.out.println("Who thinks they're really funny?");
+        String funnyman = scannerObj.nextLine();
+        System.out.println("Look out guys! " + funnyman + " thinks they're funny!!!");
     }
 }
--- src/fourteen_shop/shop.java
+++ src/fourteen_shop/shop.java
@@ -1,18 +1,60 @@
 package fourteen_shop;
 
+import java.util.Scanner;
+
 /*
 first idea: array
 put every object in an array as a string and ask for which to target
-include a way to back out of a decision
 make it so the first letter of each word in the item is not case-sensitive by including OR statements
-include comments for where to call other classes' methods such as deducting cash and displaying the inventory/
 
-assignment requires i use a switch statement,
-shouldn't be too hard and is probably exactly what i was thinking of doing anyway
+assignment requires I use a switch statement,
+shouldn't be too hard and is probably exactly what I was thinking of doing anyway
  */
 
 public class shop {
     public static void main(String[] args){
 
+        boolean exit = true;
+        String[] wares = {"Rope", "Torches", "Climbing Gear", "Clean Water", "Machete", "Canoe", "Food Supplies"};
+        Scanner selection = new Scanner(System.in);
+
+        System.out.println("You enter the Store");
+        for(String item : wares){
+            System.out.println(item);
+        }
+        System.out.println("Tell me what you'd like and I'll name it's price. If you want to leave, just say 'exit'");
+
+        while(exit){
+            String selected = selection.nextLine();
+            switch (selected){
+                case "Rope", "rope":
+                    System.out.println(selected + " would cost you 10G");
+                    break;
+                case "Torches", "torches":
+                    System.out.println(selected + " would cost you 15G");
+                    break;
+                case "Climbing Gear", "Climbing gear", "climbing Gear", "climbing gear":
+                    System.out.println(selected + " would cost you 25G");
+                    break;
+                case "Clean Water", "Clean water", "clean Water", "clean water":
+                    System.out.println(selected + " would cost you 1G");
+                    break;
+                case "Machete", "machete":
+                    System.out.println(selected + " would cost you 20G");
+                    break;
+                case "Canoe", "canoe":
+                    System.out.println(selected + " would cost you 200G");
+                    break;
+                case "Food Supplies", "Food supplies", "food Supplies", "food supplies":
+                    System.out.println(selected + " would cost you 2G");
+                    break;
+                case "Exit", "exit":
+                    System.out.println("Bye bye!");
+                    exit = false;
+                    break;
+                default:
+                    System.out.println("I didn't quite catch that. Check your spelling, maybe?");
+            }
+        }
     }
 }

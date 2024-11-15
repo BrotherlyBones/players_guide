5f911bda637e4399fd933a857c40fb3ca278bb4c
BrotherlyBones <dfmicklei@gmail.com>
2024-11-15 13:49:09 +0100

    player's guide challenge 21 its supposed to count down, not up. And i don't need the counter

--- src/twentyone_recursion/Recursion.java
+++ src/twentyone_recursion/Recursion.java
@@ -1,10 +1,9 @@
 package twentyone_recursion;
 
 public class Recursion {
-    static int x = 0;
     public static void recurse(int recursions){
-        System.out.println(x);
-        if(++x == recursions) {
+        System.out.println(recursions);
+        if(--recursions != 0) {
             recurse(recursions);
         }
     }
